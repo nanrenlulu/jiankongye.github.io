8fd66fcb961d0e73fd68aee8ca378668399542b7
nanrenlulu <47170071+nanrenlulu@users.noreply.github.com>
2022-05-07 18:41:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,14 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm791672626-9b032ccfd7be49b242c0c117',
+    'm791672645-56e10572256ea740bd025004',
+    'm791672640-adb31749798b4d4ecc91cde0',
+    'm791672638-855cc2951677f15eb07a6233',
+    'm791672620-2b7465782ddca93a45198975',
+    'm791672639-367a3f0f233b0100c6ccdba1',
+    'm791672619-d13c96ee4175312af1d9b44a',
+    'm791672617-89490cda835c975c8f678ab4',
   ],
 
   // 是否显示监测站点的链接
@@ -22,13 +26,13 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 30,
 
   // 导航栏菜单
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: '/'
     },
     {
       text: 'GitHub',
